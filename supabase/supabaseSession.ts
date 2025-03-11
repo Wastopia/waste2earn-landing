81a62922e829342fe90926404a73c70276e29a2d
kentryanentice <kentryangonzalesentice@gmail.com>
2025-03-11 15:36:42 +0800

    signup signin signout

--- supabase/supabaseSession.ts
+++ supabase/supabaseSession.ts
@@ -8,6 +8,8 @@ function supabaseSession() {
     
 
     useEffect(() => {
+        
+        let subscription
     
         const session = async () => {
             
@@ -29,13 +31,33 @@ function supabaseSession() {
                     return
                 }
 
-                setUser(sessionData)
-                setUserDetails(sessionData)
+                if (sessionData) {
+
+                    const session = sessionData.user
+
+                    const { data: userDetails, error: userDetailsError } = await supabase
+                        .from('users')
+                        .select('*')
+                        .eq('auth_id', session.id)
+                        .single()
+
+                    if (userDetailsError) {
+                        console.log('userDetailsError')
+                    }
+
+                    if (userDetails) {
+                        setUserDetails(userDetails)
+
+                    }
+                    
+                }
 
             } catch (error) {
                 console.log('error')
             } finally {
-                setLoading(false)
+                setTimeout(() => 
+                    setLoading(false), 240
+                ) 
             }
         }
     
